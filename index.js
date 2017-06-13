73647ae6ed1dabed615fc934984f27718ef26ed1
Charlie McClung <charlie@cmr1.com>
2017-06-13 16:02:07 -0600

    prototype yay

--- index.js
+++ index.js
@@ -1,11 +1,51 @@
+const gw2 = require('gw2-api');
 const VerifyBot = require('./src/VerifyBot');
+
+const api = new gw2.gw2();
+
+api.setStorage(new gw2.memStore());
 
 const bot = new VerifyBot();
 
 bot.on('connected', () => console.log('connected'));
 bot.on('ready', () => console.log('ready'));
 
+bot.cmd('whoami', (args, data) => {
+  data.user.getInfo().then(data => data.user.respond(JSON.stringify(data, null, 2)));
+});
 
+bot.cmd('hi', (args, data) => {
+  data.user.respond('Hello', data.user.get('name'));
+});
+
+bot.cmd('verify', (args, data) => {
+  api.setAPIKey(args[0]);
+
+  api.getAccount().then(res => {
+    if (res) {
+      data.user.respond('Account Info:\n', JSON.stringify(res, null, 2));      
+    } else {
+      data.user.respond('No Account Info found');
+    }
+  }).catch(err => bot.error(err));
+});
+
+    // this.api.setStorage(new gw2.memStore());
+
+// this.api.setAPIKey(data.msg);
+
+//     this.api.getAccount().then(function(res) {
+//       console.log(JSON.stringify(res, null, 2));
+//     });
+ 
+//     this.api.setAPIKey(data.msg);
+
+//     this.api.getCharacters().then(function (res) {
+//       for (var i = 0, len = res.length; i < len; i++) {
+//         // This API call just returns an array of string character names. 
+//         console.log(res[i]);
+//       }
+//     });
 // const bot = new Alfred();
 
 // bot.login('serveradmin', 'abc123')
--- src/VerifyBot.js
+++ src/VerifyBot.js
@@ -1,12 +1,8 @@
 'use strict';
 
 const async = require('async');
-const EventEmitter = require('events');
+const EventEmitter = require('eventemitter2');
 const Alfred = require('alfred-teamspeak');
-
-var gw2 = require('gw2-api');
-var api = new gw2.gw2();
-
 
 const registerEvents = [
   'server',
@@ -16,24 +12,69 @@ const registerEvents = [
   'textprivate'
 ];
 
+class BotCmd {
+  constructor({ bot, cmd, action }) {
+    this.cmd = cmd;
+    this.bot = bot;
+    this.action = action;
+
+    this.bot.on('msg', (data) => {
+      this.process(data);
+    });
+  }
+
+  process(data) {
+    if (data.msg) {
+      const parts = data.msg.replace(/\s+/g, ' ').trim().split(' ');
+
+      if (parts[0] === this.cmd) {
+        console.log('Running CMD: ' + this.cmd);
+
+        parts.shift();
+
+        this.args = Object.assign([], parts);
+
+        this.action(this.args, data);
+
+        // this.bot.emit(this.cmd, this.args);
+      }
+    }
+  }
+}
+
 class VerifyBot extends EventEmitter {
   constructor(options = {}) {
     super();
 
-    this.api = new gw2.gw2();
+    this.updateInterval = process.env.UPDATE_INTERVAL || options.updateInterval || 1000;
 
-    const connection = {
-      name: '',
-      host: '',
-      port: '',
-      sid: ''
+    this.channels = {};
+    this.serverGroups = {};
+    this.channelGroups = {};
+
+    this.commands = {};
+
+    this.connection = {
+      name: process.env.BOT_NAME || options.name || 'Alfred',
+      host: process.env.TS3_HOST || options.host || '127.0.0.1',
+      port: process.env.TS3_PORT || options.port || '10011',
+      sid:  process.env.TS3_SID  || options.sid  || '1'
     };
 
-    this.alfred = new Alfred();
+    this.credentials = {
+      user: process.env.TS3_USER || options.user || 'serveradmin',
+      pass: process.env.TS3_PASS || options.pass || 'password'
+    };
+
+    this.alfred = new Alfred(this.connection);
+    
     this.alfred.use(Alfred.User);
-    this.alfred.login('serveradmin', 'abc123')
-      .then(() => this.init())
-        .catch((err) => this.error(err));
+
+    this.alfred.login(this.credentials.user, this.credentials.pass).then(() => {
+      this.init();
+    }).catch((err) => {
+      this.error(err)
+    });
   }
 
   error(err) {
@@ -41,62 +82,181 @@ class VerifyBot extends EventEmitter {
     this.emit('error', err);
   }
 
+  join(cid) {
+    this.alfred.send('whoami', data => {
+      this.alfred.send('clientmove', { clid: data.client_id, cid });
+    });
+  }
+
   init() {
     this.emit('connected');
 
-    this.api.setStorage(new gw2.memStore());
+    this.listenChannels();
+    // this.listenServerGroups();
+    // this.listenChannelGroups();
 
-    this.registerEvents()
-      .then(() => this.emit('ready'))
-        .catch((err) => this.error(err));
+    this.registerEvents().then(() => {
+      this.emit('ready')
+    }).catch((err) => {
+      this.error(err)
+    });
 
     this.on('ready', () => {
-      this.alfred.send('channellist')
-        .then(data => this.listenChannels(data))
-        .catch(err => this.error(err));
-
       this.alfred.on('cliententerview', data => console.log(data));
-      this.alfred.on('textmessage', (data) => this.verify(data));
-      this.alfred.on('textserver', (data) => console.log(data));
-      this.alfred.on('textchannel', (data) => console.log(data));
-
-      this.alfred.on('*', function() {
-        console.log(this.event, arguments);
-      })
-      
+      this.alfred.on('textmessage', (data) => this.msg(data));
+      this.alfred.on('clientmoved', data => console.log(data));
     });
   }
 
-  listenChannels(data) {
-    // this.alfred.send('servernotifyregister', { id:data.cid[0], event: 'channel' })
-    //     .then(() => console.log('channel #'+data.cid[0]))
-    //     .catch(err => this.error(err));
-    console.log(data);
-
-    data.cid.forEach(id => {
-      this.alfred.send('servernotifyregister', { id, event: 'textchannel' })
-        .then(() => console.log('channel #'+id))
-        .catch(err => this.error(err));
-    })
+  cmd(cmd, action) {
+    if (typeof this.commands[cmd] !== 'undefined') {
+      throw new Error('cmd already registered!');
+    } else {
+      this.commands[cmd] = new BotCmd({ 
+        bot: this,
+        cmd,
+        action
+      });
+    }
   }
 
-  verify(data) {
-    console.log('verifying with data:', JSON.stringify(data, null, 2));
 
-    this.api.setAPIKey(data.msg);
+  listenChannels() {
+    setInterval(() => {
+      this.alfred.send('channellist').then((data) => {
+        const cids = Array.isArray(data.cid) ? data.cid : [ data.cid ];
 
-    this.api.getAccount().then(function(res) {
-      console.log(JSON.stringify(res, null, 2));
-    });
+        cids.forEach((cid, index) => {
+          const channel = {
+            cid: data.cid[index],
+            pid: data.pid[index],
+            name: data.channel_name[index],
+            order: data.channel_order[index],
+            clients: data.total_clients[index],
+            needed_subscribe_power: data.channel_needed_subscribe_power[index]
+          };
+
+          if (!this.channels[cid]) {
+            this.alfred.send('servernotifyregister', { event: 'textchannel', id: cid }).then(() => {
+              console.log('Listening on channel:', channel.name, cid);
+              this.channels[cid] = channel;
+            }).catch(err => this.error(err));
+          } else {
+            this.channels[cid] = Object.assign(this.channels[cid], channel);
+          }
+        });
+
+        Object.keys(this.channels).forEach((id, index) => {
+          if (cids.indexOf(id) === -1) {
+            console.log('Channel: ', this.channels[id], 'was deleted!');
+            delete this.channels[id];
+          }
+        });
+
+        // console.log(JSON.stringify(this.channels, null, 2));
+      }).catch(err => this.error(err));
+    }, this.updateInterval);
+  }
+
+  listenServerGroups() {
+    setInterval(() => {
+      this.alfred.send('servergrouplist').then((data) => {
+        console.log(data);
+
+        // const cids = Array.isArray(data.cid) ? data.cid : [ data.cid ];
+
+        // cids.forEach((cid, index) => {
+        //   const channel = {
+        //     cid: data.cid[index],
+        //     pid: data.pid[index],
+        //     name: data.channel_name[index],
+        //     order: data.channel_order[index],
+        //     clients: data.total_clients[index],
+        //     needed_subscribe_power: data.channel_needed_subscribe_power[index]
+        //   };
+
+        //   if (!this.channels[cid]) {
+        //     this.alfred.send('servernotifyregister', { id: cid, event: 'textchannel' }).then(() => {
+        //       console.log('Listening on channel:', channel.name, cid);
+        //       this.channels[cid] = channel;
+        //     }).catch(err => this.error(err));
+        //   } else {
+        //     this.channels[cid] = Object.assign(this.channels[cid], channel);
+        //   }
+        // });
+
+        // Object.keys(this.channels).forEach((id, index) => {
+        //   if (cids.indexOf(id) === -1) {
+        //     console.log('Channel: ', this.channels[id], 'was deleted!');
+        //     delete this.channels[id];
+        //   }
+        // });
+
+        // console.log(JSON.stringify(this.serverGroups, null, 2));
+      }).catch(err => this.error(err));
+    }, this.updateInterval);
+  }
+
+  listenChannelGroups() {
+    setInterval(() => {
+      this.alfred.send('channelgrouplist').then((data) => {
+        console.log(data);
+
+        // const cids = Array.isArray(data.cid) ? data.cid : [ data.cid ];
+
+        // cids.forEach((cid, index) => {
+        //   const channel = {
+        //     cid: data.cid[index],
+        //     pid: data.pid[index],
+        //     name: data.channel_name[index],
+        //     order: data.channel_order[index],
+        //     clients: data.total_clients[index],
+        //     needed_subscribe_power: data.channel_needed_subscribe_power[index]
+        //   };
+
+        //   if (!this.channels[cid]) {
+        //     this.alfred.send('servernotifyregister', { id: cid, event: 'textchannel' }).then(() => {
+        //       console.log('Listening on channel:', channel.name, cid);
+        //       this.channels[cid] = channel;
+        //     }).catch(err => this.error(err));
+        //   } else {
+        //     this.channels[cid] = Object.assign(this.channels[cid], channel);
+        //   }
+        // });
+
+        // Object.keys(this.channels).forEach((id, index) => {
+        //   if (cids.indexOf(id) === -1) {
+        //     console.log('Channel: ', this.channels[id], 'was deleted!');
+        //     delete this.channels[id];
+        //   }
+        // });
+
+        // console.log(JSON.stringify(this.channelGroups, null, 2));
+      }).catch(err => this.error(err));
+    }, this.updateInterval);
+  }
+
+  msg(data) {
+    // console.log('verifying with data:', JSON.stringify(data, null, 2));
+
+    console.log(data.targetmode);
+
+    this.emit('msg', data);      
+
+    // this.api.setAPIKey(data.msg);
+
+    // this.api.getAccount().then(function(res) {
+    //   console.log(JSON.stringify(res, null, 2));
+    // });
  
-    this.api.setAPIKey(data.msg);
+    // this.api.setAPIKey(data.msg);
 
-    this.api.getCharacters().then(function (res) {
-      for (var i = 0, len = res.length; i < len; i++) {
-        // This API call just returns an array of string character names. 
-        console.log(res[i]);
-      }
-    });
+    // this.api.getCharacters().then(function (res) {
+    //   for (var i = 0, len = res.length; i < len; i++) {
+    //     // This API call just returns an array of string character names. 
+    //     console.log(res[i]);
+    //   }
+    // });
   }
 
   registerEvents() {
