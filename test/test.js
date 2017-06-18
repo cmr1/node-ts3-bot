4710e69de454bfd5901413de53b153bdcd0b8308
Charlie McClung <charlie@cmr1.com>
2017-06-18 14:26:06 -0600

    Update test scratch script

--- test/test.js
+++ test/test.js
@@ -2,14 +2,30 @@
 
 const Bot = require('../');
 
-const bot = new Bot();
+const bot = new Bot({
+  pass: 'abc123'
+});
 
 bot.init();
 
 bot.on('ready', () => {
-  console.log('bot is ready');
+  // console.log('bot is ready');
 });
 
 bot.on('join', (channel) => {
-  console.log('bot joined channel: ', channel);
-})
+  // console.log('bot joined channel: ', channel);
+});
+
+bot.on('cliententerview', (context) => {
+  context.client.getServerGroups((err, groups) => {
+    if (err) console.error(err);
+
+    if (groups.length === 1 && groups[0].sgid === 8) {
+      context.client.message('Welcome, ' + context.client.client_nickname);
+    }
+  })
+
+  // bot._query('servergrouplist', (err, resp, req) => {
+  //   console.log(resp);
+  // })
+});
